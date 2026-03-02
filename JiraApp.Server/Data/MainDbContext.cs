18cebe9c64bbdb2bc91b26d6fda48ace4fa286a6
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2026-03-02 14:02:26 +0200

    Update MainDbContext.cs

--- JiraApp.Server/Data/MainDbContext.cs
+++ JiraApp.Server/Data/MainDbContext.cs
@@ -1,6 +1,6 @@
-﻿namespace JiraApp.Server.Data;
+namespace JiraApp.Server.Data;
 
-public class MainDbContext : DbContext
+public class MainDbContext(DbContextOptions<MainDbContext> options) : DbContext(options)
 {
     public DbSet<BoardModel> Boards { get; set; }
     public DbSet<ColumnModel> Columns { get; set; }
@@ -13,48 +13,5 @@ public class MainDbContext : DbContext
         modelBuilder.Entity<TaskModel>()
             .Property(p => p.Version)
             .IsRowVersion();
-    }
-
-    public override int SaveChanges()
-    {
-        UpdateAuditData();
-        return base.SaveChanges();
-    }
-
-    public override int SaveChanges(bool acceptAllChangesOnSuccess)
-    {
-        UpdateAuditData();
-        return base.SaveChanges(acceptAllChangesOnSuccess);
-    }
-
-    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
-    {
-        UpdateAuditData();
-        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
-    }
-
-    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
-    {
-        UpdateAuditData();
-        return base.SaveChangesAsync(cancellationToken);
-    }
-
-    private void UpdateAuditData()
-    {
-        IEnumerable<EntityEntry<BaseModel>> baseModels = ChangeTracker.Entries<BaseModel>();
-
-        foreach (EntityEntry<BaseModel> entry in baseModels)
-        {
-            if (entry.State == EntityState.Added)
-            {
-                entry.Entity.CreatedAt = DateTime.UtcNow;
-                entry.Entity.Id = Guid.CreateVersion7();
-            }
-
-            if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
-            {
-                entry.Entity.UpdatedAt = DateTime.UtcNow;
-            }
-        }
     }
 }
