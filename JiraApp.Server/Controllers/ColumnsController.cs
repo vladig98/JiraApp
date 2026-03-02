d66c225ab5b080cb2c1ecbe779bcd75641fbf598
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2026-03-02 14:01:45 +0200

    Add files via upload

---
+++ JiraApp.Server/Controllers/ColumnsController.cs
@@ -0,0 +1,30 @@
+﻿namespace JiraApp.Server.Controllers;
+
+[Route("columns")]
+[ApiController]
+public class ColumnsController : ControllerBase
+{
+    [HttpPost("/boards/{boardId:Guid}/columns")]
+    public async Task<IActionResult> Create(Guid boardId, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id:Guid}")]
+    public async Task<IActionResult> Update(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("reorder")]
+    public async Task<IActionResult> Reorder(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ JiraApp.Server/Controllers/TasksController.cs
@@ -0,0 +1,36 @@
+﻿namespace JiraApp.Server.Controllers;
+
+[Route("tasks")]
+[ApiController]
+public class TasksController : ControllerBase
+{
+    [HttpPost("/columns/{columnId:Guid}/tasks")]
+    public async Task<IActionResult> Create(Guid columnId, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("{id:Guid}")]
+    public async Task<IActionResult> Update(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("move")]
+    public async Task<IActionResult> Move(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut("reorder")]
+    public async Task<IActionResult> Reorder(Guid id, CancellationToken ct)
+    {
+        throw new NotImplementedException();
+    }
+}
